4dafb71778511e2fd83d13a3b78e7f3194098911
s2010 <bashayer.hijji@gmail.com>
2016-12-29 10:05:02 +0300

    adding  Friends table migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,19 @@ class User extends Authenticatable
     {
         return "https://www.gravatar.com/avatar/{{ md5($this->email)}}?d=mm&s=40";
     }
+
+    public function friendsOfMine()
+    {
+        return $this->belongsToMany('Chatty\Models\User','friends','user_id','friend_id');
+    }
+
+    public function friendOf()
+    {
+        return $this->belongsToMany('Chatty\Models\User','friends','friend_id','user_id');
+    }
+
+    public function  friends()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted',true)->get()->merge($this->friendOf()->wherePivot('accepted', true)->get());
+    }
 }
