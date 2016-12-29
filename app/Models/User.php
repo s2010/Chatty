1faa74beb78bafc2cd2e1e82d56a7acd7f5ad4b9
s2010 <bashayer.hijji@gmail.com>
2016-12-29 11:10:06 +0300

    Friend model methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -86,4 +86,35 @@ class User extends Authenticatable
     {
         return $this->friendsOfMine()->wherePivot('accepted',false)->get();
     }
+
+    public function friendRequestsPending()
+    {
+        return $this->friendOf()->wherePivot('accepted',false)->get();
+    }
+
+    public function hasFriendRequestsPending(User $user)
+    {
+        return (bool) $this->friendRequestsPending()->where('id',$user->id)->count();
+    }
+
+    public function hasFriendRequestsReceived(User $user)
+    {
+        return (bool) $this->friendRequests()->where('id',$user->id)->count();
+    }
+
+    public function addFriend(User $user)
+    {
+        $this->friendOf()->attach($user->id);
+    }
+    public function acceptFriendRequest(User $user)
+    {
+        $this->friendRequests()->where('id',$user->id)->first()->pivot->update([
+            'accepted' => true,
+        ]);
+    }
+
+    public function isFriendsWith(User $user)
+    {
+        return (bool) $this->friends()->where('id', $user->id)->count();
+    }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,6 +7,16 @@
           <hr>
       </div>
       <div class="col-lg-4 col-lg-offset-3">
+          @if(Auth::user()->hasFriendRequestsPending($user))
+              <p>Waiting for {{$user->getFirstNameOrUsername()}} to accept your request.</p>
+          @elseif(Auth::user()->hasFriendRequestsReceived($user))
+              <a href="#" class="btn btn-primary">Accept friend request</a>
+          @elseif(Auth::user()->isFriendsWith($user))
+              <p>You & {{$user->getFirstNameOrUsername()}} are friends.</p>
+          @else
+              <a href="#" class="btn btn-primary">Add as friend</a>
+          @endif
+
           <h4>{{$user->getFirstNameOrUsername()}}'s friends</h4>
 
           @if(!$user->friends()->count())
