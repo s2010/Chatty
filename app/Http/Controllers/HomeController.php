8d0e2e5499055cb9eb4dad36bd8aaa8d97ebeef3
s2010 <bashayer.hijji@gmail.com>
2016-12-31 12:16:15 +0300

    showing statuses on the timeline

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace Chatty\Http\Controllers;
 
+use Chatty\Models\Status;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -10,7 +11,15 @@ class HomeController extends Controller
     {
         if (Auth::check())
         {
-            return view('timeline.index');
+            $statuses = Status::where(function($query){
+                return $query->where('user_id', Auth::user()->id)
+                    ->orWhereIn('user_id', Auth::user()->friends()->lists('id'));
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+            return view('timeline.index')
+                ->with('statuses',$statuses);
         }
         return view('home');
     }
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -19,7 +19,47 @@
 
     <div class="row">
         <div class="col-lg-5">
+            @if(!$statuses->count())
+                <p>There's nothing in your timeline, yet.</p>
+            @else
+              @foreach($statuses as $status)
+                  <div class="media">
 
+                      <a class="pull-left" href="{{route('profile.index', ['username' =>$status->user->username])}}">
+                          <img class="media-object" alt="{{$status->user->getNameOrUsername()}}" src="{{$status->user->getAvaterUrl()}}">
+                      </a>
+
+                      <div class="media-body">
+                          <h4 class="media-heading"><a href="{{route('profile.index', ['username' =>$status->user->username])}}">{{$status->user->getNameOrUsername()}}</a></h4>
+                          <p>{{$status->body}}</p>
+                          <ul class="list-inline">
+                              <li>{{$status->created_at->diffForHumans()}}</li>
+                          </ul>
+
+                          {{--<div class="media">--}}
+                              {{--<a class="pull-left" href="#">--}}
+                                  {{--<img class="media-object" alt="" src="">--}}
+                              {{--</a>--}}
+                              {{--<div class="media-body">--}}
+                                  {{--<h4 class="media-heading"><a href="#"></a></h4>--}}
+                                  {{--<p>It's a lovely day</p>--}}
+                                  {{--<ul class="list-inline">--}}
+                                      {{--<li></li>--}}
+                                  {{--</ul>--}}
+                              {{--</div>--}}
+                          {{--</div>--}}
+
+                          <form role="form" action="#" method="post">
+                              <div class="form-group">
+                                  <textarea name="reply-1" class="form-control" role="2" placeholder="Reply to this status"></textarea>
+                              </div>
+                              <input type="submit" value="Reply" class="btn btn-default btn-sm">
+                          </form>
+                      </div>
+                  </div>
+              @endforeach
+                {!! $statuses->render() !!}
+            @endif
         </div>
     </div>
 @stop
